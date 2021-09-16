5b78376c622dea96ad4a25127e026fdc734694a3
hharishma8 <90748443+hharishma8@users.noreply.github.com>
2021-09-16 11:42:38 +0530

    python class file upload

---
+++ class and object student.py
@@ -0,0 +1,14 @@
+class stu:
+    def dis():
+        Sname=input("Enter your name:")
+        Rno=input("Enter your rno:")
+        Mark=input("Enter your mark:")
+        Total=input("Enter your total:")
+        Average=input("Enter your average:")
+        print("name",Sname)
+        print("rno",Rno)
+        print("mark",Mark)
+        print("total",Total)
+        print("average",Average)
+s1=stu
+s1.dis()
